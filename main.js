b263dc75cf10bb21eb4acb0827eea4cfd80a051f
Burger-get-down <157125385+Burger-get-down@users.noreply.github.com>
2025-03-25 03:12:02 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+//темная тема
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn__active");
+    document.body.classList.toggle('dark')
+}
+
+
+//живщй инрут бренда
+document.querySelector('#elastic').oninput = function(){
+    let val = this.value.trim();
+    let elasticItems = document.querySelectorAll('.elastic li');
+    if(val !='') {
+        elasticItems.forEach(function(elem){
+            if(elem.innerText.search(val) == -1){
+                elem.classList.add('hide');
+            }
+            else {
+                elem.classList.remove('hide');
+            }
+        })
+    }
+    else {
+        elasticItems.forEach(function(elem){
+            elem.classList.remove('hide');
+    })
+    }
+}
